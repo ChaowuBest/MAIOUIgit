621e8483ad2b71f415d096b249a2c6c54feea373
多啦S梦 <1553992118@qq.com>
2020-08-01 20:24:15 +0800

    -updateV0.99.3 -fix price bug -Add Advance(select account) button

--- MAIO/LoginWindow.xaml.cs
+++ MAIO/LoginWindow.xaml.cs
@@ -21,11 +21,11 @@ using System.Windows.Shapes;
 namespace MAIO
 {
     /// <summary>
-    /// version 0.99.2
+    /// version 0.99.3
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public static string version = "0.99.2";//everychange
+        public static string version = "0.99.3";//everychange
         public LoginWindow()
         {
             InitializeComponent();
