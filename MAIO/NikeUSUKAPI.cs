05f620d68fc76a6c9c92fb887ef822682f9013bd
多啦S梦 <1553992118@qq.com>
2020-08-12 09:36:24 +0800

    update

--- MAIO/NikeUSUKAPI.cs
+++ MAIO/NikeUSUKAPI.cs
@@ -1125,6 +1125,7 @@ namespace MAIO
                 if (status.Contains("IN_PROGRESS") == true)
                 {
                     tk.Status = "Processing";
+                    goto A;
                 }
                 if (status.Contains("COMPLETED") == true)
                 {
