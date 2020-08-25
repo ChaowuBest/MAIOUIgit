8a4725067ba764d38c07bd7e724d2f2f7a9e7251
多啦S梦 <1553992118@qq.com>
2020-08-25 13:34:30 +0800

    update V0.99.27 -Add Watch dog task - optimization task -bugs fix

--- MAIO/LoginWindow.xaml.cs
+++ MAIO/LoginWindow.xaml.cs
@@ -22,11 +22,11 @@ using System.Windows.Shapes;
 namespace MAIO
 {
     /// <summary>
-    /// version 0.99.26
+    /// version 0.99.27
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public static string version = "0.99.26";//everychange
+        public static string version = "0.99.27";//everychange
         public LoginWindow()
         {
             InitializeComponent();
