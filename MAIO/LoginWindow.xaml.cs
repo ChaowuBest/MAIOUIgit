bfafa4e3ea2a79868964409508a3929d5987f42a
多啦S梦 <1553992118@qq.com>
2020-09-10 04:28:44 +0800

    update

--- MAIO/LoginWindow.xaml.cs
+++ MAIO/LoginWindow.xaml.cs
@@ -22,11 +22,11 @@ using System.Windows.Shapes;
 namespace MAIO
 {
     /// <summary>
-    /// version 0.99.38
+    /// version 0.99.39
     /// </summary>
     public partial class LoginWindow : Window
     {
-        public static string version = "0.99.38";//everychange
+        public static string version = "0.99.39";//everychange
         public LoginWindow()
         {
             InitializeComponent();
